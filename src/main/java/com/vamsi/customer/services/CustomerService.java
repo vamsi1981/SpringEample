218d978be90d50215bd2d51d9fa6f4236364e872
Vamsi Madiraju <vamsi.futurama@gmail.com>
2019-10-01 02:18:00 -0400

    my first checkin

---
+++ src/main/java/com/vamsi/customer/services/CustomerService.java
@@ -0,0 +1,14 @@
+package com.vamsi.customer.services;
+
+public class CustomerService 
+{
+	String message;
+	
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
